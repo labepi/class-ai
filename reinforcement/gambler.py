df7843b9407a3dbb4607434b0f103379934bd16c
Joao Medeiros <jpsm1985@gmail.com>
2023-07-04 11:05:50 -0300

    Adds Gambler's problem optimal policies

--- reinforcement/gambler.py
+++ reinforcement/gambler.py
@@ -20,12 +20,11 @@ def value_iteration(p=0.5, theta=0.000000001):
             delta = max(delta, abs(v - value[state]))
     return value
 
-def optimal_policy(value, theta=0.000000001):
+def optimal_policy(value, p=0.5, theta=0.000000001):
     """
     """
     policy = {}
     for state in range(1, 100):
-        v = value[state]
         max_action = [(0, 1)]
         for action in range(1, min(state, 100 - state) + 1):
             r = p * value[state + action] + (1 - p) * value[state - action]
@@ -43,13 +42,27 @@ def flip(p):
         return True
     return False
 
-def player_rand(state=1, p=0.5):
+def player_optimal(policy, state=1, p=0.5):
+    """
+    """
+    while (0 < state and state < 100):
+        choice = random.randint(0, len(policy[state]) - 1)
+        amount = policy[state][choice][1]
+        coin = flip(p)
+        #print(">> state", state, "amount", amount, "coin", coin, "all", policy[state])
+        if coin:
+            state = state + amount
+        else:
+            state = state - amount
+    return state
+
+def player_random(state=1, p=0.5):
     """
     """
     while (0 < state and state < 100):
         amount = random.randint(1, min(state, 100 - state))
         coin = flip(p)
-        print(">> state", state, "amount", amount, "coin", coin)
+        #print(">> state", state, "amount", amount, "coin", coin)
         if coin:
             state = state + amount
         else:
@@ -60,13 +73,21 @@ if __name__ == "__main__":
     """
     state = int(sys.argv[1])
     p = float(sys.argv[2])
-    print(player(state, p))
+    print(player_rand(state, p))
+    """
+
     """
     p = float(sys.argv[1])
 
     value = value_iteration(p)
-    policy = optimal_policy(value)
-
+    policy = optimal_policy(value, p)
     for state in policy:
         for action in policy[state]:
             print(state, action[1])
+    """
+    state = int(sys.argv[1])
+    p = float(sys.argv[2])
+    value = value_iteration(p)
+    policy = optimal_policy(value, p)
+    #print(player_optimal(policy, state, p))
+    print(player_random(state, p))
